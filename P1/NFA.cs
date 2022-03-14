618ac1801d8fac1491db548c3e033c4770672f9b
Aylin Naebzadeh <aylin.naebzadeh@gmail.com>
2022-03-15 01:08:56 +0330

    Transition class

---
+++ P1/NFA.cs
@@ -0,0 +1,17 @@
+namespace P1
+{
+    public class NFA
+    {
+        /*
+            one list for states
+            one list for alphabets
+            one list for final states
+            list for transitions
+            The number of transitions
+        */
+        public NFA()
+        {
+
+        }
+    }
+}
